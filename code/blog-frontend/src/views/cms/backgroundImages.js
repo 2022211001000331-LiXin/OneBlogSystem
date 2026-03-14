62a61f6d7a2e18e0c36c6620fb51484465682eaa
justina <2145143087@qq.com>
2026-03-14 17:18:53 +0800

    第一次提交

---
+++ code/blog-frontend/src/views/cms/backgroundImages.js
@@ -0,0 +1,21 @@
+// 背景图片随机生成
+import my_bg1 from '../../assets/images/my_bg1.jpg'
+import my_bg2 from '../../assets/images/my_bg2.jpg'
+import my_bg3 from '../../assets/images/my_bg3.jpg'
+import my_bg4 from '../../assets/images/my_bg4.jpg'
+import my_bg5 from '../../assets/images/my_bg5.jpg'
+import my_bg6 from '../../assets/images/my_bg6.jpg'
+import my_bg7 from '../../assets/images/my_bg7.jpg'
+import my_bg8 from '../../assets/images/my_bg8.jpg'
+
+export default[
+	my_bg1,
+	my_bg2,
+	my_bg3,
+	my_bg4,
+	my_bg5,
+	my_bg6,
+	my_bg7,
+	my_bg8
+
+]
